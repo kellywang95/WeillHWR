853114c170cb485b749575931badf082d29140af
kelly wang <kellywang.suju@gmail.com>
2017-11-16 22:10:31 -0500

    updating index.html

--- crnn.pytorch/flaskr.py
+++ crnn.pytorch/flaskr.py
@@ -10,7 +10,29 @@ app = Flask(__name__)  # create the application instance :)
 
 @app.route('/')
 def show_entries():
-    return 'Now it is time to start working on the templates. As you may have noticed, if you make requests with'
+    return render_template('index.html')
+
+# @app.route('/login.html')
+# def showLogin():
+#     return render_template('login.html')
+#
+# @app.route('/kelly')
+# def show_home():
+#     return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
 
 
 @app.route('/image_txt', methods=['POST'])
