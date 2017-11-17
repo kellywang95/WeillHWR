673fe92c27c20ee6a1a0f4c50ec479f0a017fb12
rohun-tripati <tripati.riot@gmail.com>
2017-11-16 22:41:02 -0500

    backedn for the search thing

--- crnn.pytorch/flaskr.py
+++ crnn.pytorch/flaskr.py
@@ -20,29 +20,30 @@ def show_entries():
 # def show_home():
 #     return render_template('index.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('show_entries'))
+        # Not actually checking any
+        return redirect(url_for('show_entries'))
     return render_template('login.html', error=error)
 
 
 @app.route('/image_txt', methods=['POST'])
 def image_txt():
-    print(request.form)
-    print(request)
     if 'index' not in request.form:
         raise Exception("Send me the Index!")
 
     return run_for_given_file.extract_result(request.form['index'])
+
+
+@app.route('/search_txt', methods=['POST'])
+def image_txt():
+    if 'keyword' not in request.form:
+        raise Exception("Send me the keyword!")
+
+    return 'Blah Blah Blah for now'
 
 
 app.run()
